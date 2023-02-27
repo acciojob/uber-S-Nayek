db88e602639a266aa09a8558e7ceb3fb6e31e68d
S-Nayek <shirshendu.nayek96@gmail.com>
2023-02-27 12:49:45 +0530

    1st

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -1,7 +1,11 @@
 package com.driver.controllers;
 
-import com.driver.services.AdminService;
+import com.driver.model.Admin;
+import com.driver.model.Customer;
+import com.driver.model.Driver;
+import com.driver.services.impl.AdminServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,28 +15,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	AdminServiceImpl adminServiceImpl;
+
 
 	@PostMapping("/register")
 	public ResponseEntity<Void> registerAdmin(@RequestBody Admin admin){
+		adminServiceImpl.adminRegister(admin);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
-		return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
+		Admin admin=adminServiceImpl.updatePassword(adminId,password);
+		return new ResponseEntity<>(admin, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete")
 	public void deleteAdmin(@RequestParam Integer adminId){
+		adminServiceImpl.deleteAdmin(adminId);
 	}
 
 	@GetMapping("/listOfCustomers")
 	public List<Customer> listOfCustomers() {
-		return listOfCustomers;
+		return adminServiceImpl.getListOfCustomers();
 	}
+
+
 
 	@GetMapping("/listOfDrivers")
 	public List<Driver> listOfDrivers() {
-		return listOfDrivers;
+		return adminServiceImpl.getListOfDrivers();
 	}
 }
